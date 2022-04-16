dfc69e9ff21a3d4a43d00fedf1415de1bf7fd271
Semiz Software <semisft@gmail.com>
2022-04-16 11:35:28 +0300

    Recognize stop action

--- src/main/java/com/semiz/batch/HelloBatch.java
+++ src/main/java/com/semiz/batch/HelloBatch.java
@@ -1,17 +1,29 @@
 package com.semiz.batch;
 
 import javax.batch.api.AbstractBatchlet;
+import javax.batch.runtime.BatchStatus;
 
 public class HelloBatch extends AbstractBatchlet {
+	
+	private boolean stopRequested = false;
 
 	@Override
 	public String process() throws Exception {
 		System.out.println("hello world");
-		return null;
+		for (int i=0;i<5;i++) {
+			System.out.println("Step "+i+" is being processed.");
+			Thread.sleep(5000);
+			if (stopRequested) {
+				System.out.println("stopped");
+				return BatchStatus.STOPPED.toString();
+			}
+		}
+		return BatchStatus.COMPLETED.toString();
 	}
 	
 	@Override
 	public void stop() throws Exception {
+		stopRequested = true;
 		System.out.println("stop requested");
 	}
 
